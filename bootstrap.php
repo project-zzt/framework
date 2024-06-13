bd2d0cb809b872afe53fd798722588c505ff8b91
cris-the-creator <contact@corneascorner.dev>
2024-06-13 19:34:39 +0200

    merge conflict

--- bootstrap.php
+++ bootstrap.php
@@ -2,14 +2,8 @@
 
 use zzt\Core\Application;
 
-<<<<<<< HEAD
-use function zzt\web\init;
-
-init();
-=======
 function run(array $config, array $modules)
 {
   $app = Application::init($config, $modules);
   $app->run();
 }
->>>>>>> 59183569d48757b04822acf7fe6b0d25bd17f626
