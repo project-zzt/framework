46806ef3563af068c8a972c203223f528428a2cc
cris-the-creator <contact@corneascorner.dev>
2024-06-14 19:00:13 +0200

    fix config

--- .history/globals/tools_20240614183633.php
+++ .history/globals/tools_20240614183633.php
@@ -9,7 +9,7 @@ use zzt\Exception\ConfigException;
 function build_module_path(array $config): array
 {
   $basePath = $config['base_path'] ?? throw new ConfigException('base_path');
-  $moduleDir = $config['base']['modules_dir'] ?? throw new ConfigException('modules_dir');
+  $moduleDir = $config['base']['module_dir'] ?? throw new ConfigException('module_dir');
 
   $folders = scandir($basePath . '/' . $moduleDir);
 
