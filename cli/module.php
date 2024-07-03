ed8912f19adf32220da06f4b489a314c13d6a06e
cris-the-creator <contact@corneascorner.dev>
2024-07-03 11:22:22 +0200

    module cli finish

--- cli/module.php
+++ cli/module.php
@@ -9,11 +9,24 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Application;
 
+$appDir = __DIR__ . '/../../app';
+$config = require $appDir . '/config/config.php';
+
+$appName = $config['base']['app_name'];
+$modulesDir = $config['base']['modules_folder'];
+
 class ModuleCli extends Command
 {
-  public function __construct(public readonly string $appName, public readonly string $moduleFolder)
-  {
+  private readonly string $templates;
+
+  public function __construct(
+    private readonly string $appDir,
+    private readonly string $appName,
+    private readonly string $modulesDir,
+  ) {
     parent::__construct();
+
+    $this->templates = __DIR__ . '/../templates';
   }
 
   protected function configure(): void
@@ -27,23 +40,70 @@ class ModuleCli extends Command
 
   protected function execute(InputInterface $input, OutputInterface $output): int
   {
+    // Create folders
+    $newModule = $input->getArgument('name');
+    $path = $this->appDir . '/' . $this->modulesDir . '/' . $newModule;
+    if (is_dir($path)) {
+      $output->writeln(sprintf('Folder %s already exists.', $newModule));
+      return Command::FAILURE;
+    }
+    // Module folder
+    $ok = mkdir($path);
+    if (! $ok) {
+      $output->writeln('Could not create module folder, something went wrong.');
+      return Command::FAILURE;
+    }
+    // Handler folder
+    $ok = mkdir($path . '/handlers');
+    if (! $ok) {
+      $output->writeln('Could not create handlers folder, something went wrong.');
+      return Command::FAILURE;
+    }
+    // Views folder
+    $ok = mkdir($path . '/views');
+    if (! $ok) {
+      $output->writeln('Could not create views folder, something went wrong.');
+      return Command::FAILURE;
+    }
+
+    // Copy templates
+    $from = $this->templates;
+    $to = $this->appDir . '/' . $this->modulesDir . '/' . $newModule;
+    // Module template
+    $moduleTemaplte = $to . '/module.php';
+    $ok = copy($from . '/module/module.dist.php', $moduleTemaplte);
+    if (! $ok) {
+      $output->writeln('Could not copy module file, something went wrong.');
+      return Command::FAILURE;
+    }
+    // Handler template
+    $handlerTemplate = $to . sprintf('/handlers/%s.php', $newModule);
+    $ok = copy($from . '/module/handler.dist.php', $handlerTemplate);
+    if (! $ok) {
+      $output->writeln('Could not copy handler file, something went wrong.');
+      return Command::FAILURE;
+    }
+
+    // Resolve placeholders
+    $file = file_get_contents($moduleTemaplte);
+    file_put_contents($moduleTemaplte, str_replace('{{APP_NAME}}', $this->appName, $file));
+    file_put_contents($moduleTemaplte, str_replace('{{MODULE_NAME}}', $newModule, $file));
+    $file = file_get_contents($handlerTemplate);
+    file_put_contents($moduleTemaplte, str_replace('{{APP_NAME}}', $this->appName, $file));
+    file_put_contents($moduleTemaplte, str_replace('{{MODULE_NAME}}', $newModule, $file));
+
+    // Output
     $output->writeln(
       sprintf(
         'Created module "%s" in app/%s',
-        $input->getArgument('name'),
-        $this->moduleFolder
+        $newModule,
+        $this->modulesDir
       )
     );
     return Command::SUCCESS;
   }
 }
 
-$appDir = __DIR__ . '/../../app';
-$config = require $appDir . '/config/config.php';
-
-$appName = $config['base']['app_name'];
-$modulesDir = $config['base']['modules_folder'];
-
 $app = new Application();
-$app->add(new ModuleCli($appName, $modulesDir));
+$app->add(new ModuleCli($appDir, $appName, $modulesDir));
 $app->run();
